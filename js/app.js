045a6b956953cd0ebf8e576e22a4b9995e82b735
Tumilo-Denisovich Vladimir <Tumilo.Denisovich@gmail.com>
2023-04-26 20:44:31 +0400

    feat: visual click keys, change register onclick shift

--- js/app.js
+++ js/app.js
@@ -1,1 +1,76 @@
-console.log('test')
+import {
+  codeKey, engLowerCase, engUpperCase, rusLowerCase, rusUpperCase, isEng
+} from './keyLayout.js';
+
+function getCL(data) {
+  return console.log(data);
+}
+
+getCL('test');
+
+const allBtn = document.querySelectorAll('.key');
+
+function generationKeys(layout) {
+  for (let i = 0; i < allBtn.length; i += 1) {
+    allBtn[i].textContent = layout[i];
+  }
+}
+
+function clickShift(position, value) {
+  if (value === 'down') {
+    if (position === 'left') {
+      allBtn[42].classList.add('key_active');
+    } else {
+      allBtn[55].classList.add('key_active');
+    }
+    if (isEng.boo === true) {
+      generationKeys(engUpperCase);
+    } else {
+      generationKeys(rusUpperCase);
+    }
+  } else if (value === 'up') {
+    if (isEng.boo === true) {
+      generationKeys(engLowerCase);
+    } else {
+      generationKeys(rusLowerCase);
+    }
+  };
+}
+
+const keyboard = document.querySelector('.body');
+
+keyboard.addEventListener('keydown', (event) => {
+  const clickKey = event.code;
+  const indexPressKey = codeKey.findIndex((key) => key === clickKey);
+  getCL(indexPressKey);
+
+  console.log(clickKey);
+
+  allBtn[indexPressKey].classList.add('key_active');
+
+  if (clickKey === 'ShiftLeft') {
+    clickShift('left', 'down');
+  } else if (clickKey === 'ShiftRight') {
+    clickShift('right', 'down');
+  }
+});
+
+function clearKey(event) {
+  const clickKey = event.code;
+  const indexPressKey = codeKey.findIndex((key) => key === clickKey);
+  allBtn[indexPressKey].classList.remove('key_active');
+}
+
+keyboard.addEventListener('keyup', (event) => {
+  clearKey(event);
+  const clickKey = event.code;
+  if (clickKey === 'ShiftLeft') {
+    clickShift('left', 'up');
+  } else if (clickKey === 'ShiftRight') {
+    clickShift('right', 'up');
+  }
+});
+
+export {
+  generationKeys,
+};
---
+++ js/keyLayout.js
@@ -0,0 +1,12 @@
+const codeKey = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Delete', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Backslash', 'Enter', 'ShiftLeft', 'IntlBackslash', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'AltLeft', 'MetaLeft', 'Space', 'MetaRight', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+const engLowerCase = ['§', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'Del', '⇪ CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '\\', 'Enter', '⇧ Shift', '`', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', '⇧ Shift', 'Control', 'Option', 'Command', 'Space', 'Command', 'Option', '←', '↓', '→'];
+const engUpperCase = ['±', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', 'Del', '⇪ CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '|', 'Enter', '⇧ Shift', '~', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '↑', '⇧ Shift', 'Control', 'Option', 'Command', 'Space', 'Command', 'Option', '←', '↓', '→'];
+const rusLowerCase = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'Del', '⇪ CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', '\\', 'Enter', '⇧ Shift', ']', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '↑', '⇧ Shift', 'Control', 'Option', 'Command', 'Space', 'Command', 'Option', '←', '↓', '→'];
+const rusUpperCase = ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', 'Del', '⇪ CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', '/', 'Enter', '⇧ Shift', '[', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '↑', '⇧ Shift', 'Control', 'Option', 'Command', 'Space', 'Command', 'Option', '←', '↓', '→'];
+const isEng = {
+  boo: true,
+};
+
+export {
+  codeKey, engLowerCase, engUpperCase, rusLowerCase, rusUpperCase, isEng,
+};
